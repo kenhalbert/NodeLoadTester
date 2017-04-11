faf1bd6e4b6fd4a24a81b556224e40410ebac1ba
Ken Halbert <ken.halbert@nordstrom.com>
2017-04-11 16:43:36 +0000

    Added example requestor

---
+++ requestors/example.js
@@ -0,0 +1,19 @@
+const request = require('superagent');
+
+module.exports = () => {
+
+	// do anything you need to here - just make sure to return an ES6 promise
+
+	return new Promise((resolve, reject) => {
+		const url = 'www.google.com';
+
+		request
+		  .get(url)
+		  .end((err, res) => {
+		    if (err) 
+		    	reject(err);
+		    else 
+		    	resolve(res);
+		  });
+	});
+};
